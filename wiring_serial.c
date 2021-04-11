3c33b53b738cf3d739a7190e6eb3ee031d8f0855
Evan Douglas Foglio <evan.foglio@maine.edu>
2021-04-11 11:57:29 -0400

    fixed file output, opens and closes file every loop

--- wiring_serial.c
+++ wiring_serial.c
@@ -2,49 +2,81 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <termios.h>
 #include <string.h>
+int init(void);
 int main()
 {
 	//serial descriptor
 	int fd;
-	int input_len;
-	char buffer[41];
-	int i;
-	int fd2;
-	
+	char buffer[42];
+	FILE *fd2 = NULL;
+	FILE *serial_in;
+	int fdserial = init();	
 	//open serial port
-	fd  = serialOpen("/dev/serial0", 9600);	
+	fd = serialOpen("/dev/serial0", 9600);	
 	//serial port error checking
 	if(fd < 0){
 		printf("Could not open serial port 0\n");
 		return -1;
 	}
+
+	//fd2 = fopen("rail_voltages.dat","w");
+
+	//if(fd2==NULL){
+	//	printf("Error opening output file\n");
+	//	return -1;
+	//}
+	serial_in = fdopen(fdserial, "r");
+	
 	usleep(2000000);// let serial sort itself out
 	
 	//clear all awaiting data
 	serialFlush(fd);
 	
-	//open outpur file
-	fd2 = open("rail_voltages.dat", O_WRONLY);
-	//output file error checking
-	if(fd2 < 0){
-		printf("cannot open output file \"rail_voltages.dat\"");
-		return -1;
+
+	while(fgets(buffer, 42, serial_in)){
+		printf("%s", buffer);
+		
+		fd2 = fopen("rail_voltages.dat","a");
+
+	        if(fd2==NULL){
+        	        printf("Error opening output file\n");
+        	        return -1;
+        	}
+
+		fprintf(fd2, "%s", buffer);
+
+		fclose(fd2);
+
+
 	}
-	
-	while(1){
-		input_len = serialDataAvail(fd);
-		if(input_len > 41)
-			input_len = 41;
-		//print input string
-		for(i = 0; i < input_len; i++){
-			buffer[i] = serialGetchar(fd);
-			printf("%c", buffer[i]);
-		}
-	}	
-
-	close(fd2);
 	close(fd);
+}
 
-	
+
+
+int init()
+{
+	int fd1;
+	struct termios tc; // terminal control structure
+
+	//todo serial port should not be hard coded
+	fd1 = open("/dev/serial0", O_RDWR | O_NOCTTY);
+	if (fd1 < 1) {
+		printf("Failed to open serial port\n");
+		return 0;
+	}
+	tcgetattr(fd1, &tc);
+	tc.c_iflag = IGNPAR;
+	tc.c_oflag = 0;
+	tc.c_cflag = CS8 | CREAD | CLOCAL; //8 bit chars enable receiver no modem status lines
+	tc.c_lflag = ICANON;
+
+	//todo baud rate should not be hard coded
+	cfsetispeed(&tc, B9600);
+	cfsetospeed(&tc, B9600);
+	//todo should have bits per character set
+	tcsetattr(fd1, TCSANOW, &tc);
+	return fd1;
 }
